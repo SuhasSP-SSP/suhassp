fe4c8f9b0967dc57aecfc770b0cd7c5ec8f30ed2
Suhas S P <73066709+SuhasSP-SSP@users.noreply.github.com>
2021-12-20 14:45:39 +0530

    Update script.js

--- auth/script.js
+++ auth/script.js
@@ -29,6 +29,6 @@ $('form').submit(function(){
     $('.tick').css('transform','scale(1) translateY(-50%)')
   },200);
   },3650);
-  setTimeout(() => {  window.location = "https://bit.ly/MasterAdminDashboard" }, 8500);
+  setTimeout(() => {  window.location = "https://bit.ly/MasterAdminDashboard" }, 6400);
   return false;
 })
